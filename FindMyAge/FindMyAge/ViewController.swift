7f571436b13e33d8b726d1f41ef4ee21e5386413
Astrid28 <49130603+Astrid28@users.noreply.github.com>
2019-03-31 16:42:45 -0600

    Add files via upload

---
+++ FindMyAge/FindMyAge/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  FindMyAge
+//
+//  Created by UTNG on 08/02/19.
+//  Copyright © 2019 UTNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var laShowAge: UILabel!
+    
+    @IBOutlet weak var txtYearOfBirth: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+      
+        
+        
+    }
+
+  
+
+    @IBAction func BuFindDage(_ sender: Any) {
+    }
+    //fire click of button
+    let yearOfBirth    
+}
+
